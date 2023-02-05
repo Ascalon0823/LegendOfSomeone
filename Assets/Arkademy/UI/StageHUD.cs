ba9aaad8e6b3654668b43c50cbda942b6d5cfdb5
Ascalon <ascalon0823@gmail.com>
2023-02-05 20:01:38 +0800

    add stage

--- Assets/Arkademy/UI/StageHUD.cs
+++ Assets/Arkademy/UI/StageHUD.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class StageHUD : MonoBehaviour
+namespace Arkademy.UI
 {
-    // Start is called before the first frame update
-    void Start()
+    public class StageHUD : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+       
     }
 }
